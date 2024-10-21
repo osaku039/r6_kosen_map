9c16b38c8a1cc72b94cd9c99aa7bf58ed720342d
osaku039 <booklove039@gmail.com>
2024-10-21 12:04:30 +0900

    programInformationに名称変更

--- js/souzou.js
+++ js/souzou.js
@@ -1,5 +1,5 @@
 import { locateInfo } from './locateInformation.js';
-import { classInfo } from './classInformation.js';
+import { classInfo } from './programInformation.js';
 console.log('main.js is loaded'); // ファイルロード確認用のログ
 
 // シーン、カメラ、レンダラーのセットアップ
